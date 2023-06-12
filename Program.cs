4aebd99f76f98c75f9f80bbe36c48c75fe1cdd06
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 10:43:49 +0300

    t1 valmis

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Luo ohjelma, joka luo kokonaislukumuuttujan 
+ * nimeltä "ika" ja antaa sille arvoksi 25. 
+ * Näytä muuttujan arvo konsolissa.
+ */
+int ika = 25;
+Console.WriteLine(ika);
+Console.WriteLine("Ikä = " + ika);
