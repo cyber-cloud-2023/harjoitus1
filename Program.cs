dd345fffe8f91e0185f19267e67df4f46d1549ca
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 10:47:49 +0300

    t2 valmis

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*
+/* Tehtävä1
  * Luo ohjelma, joka luo kokonaislukumuuttujan 
  * nimeltä "ika" ja antaa sille arvoksi 25. 
  * Näytä muuttujan arvo konsolissa.
@@ -7,3 +7,11 @@
 int ika = 25;
 Console.WriteLine(ika);
 Console.WriteLine("Ikä = " + ika);
+
+/*Tehtävä2
+ * Luo ohjelma, joka luo merkkijonomuuttujan nimeltä "nimi" ja antaa sille oman nimesi. 
+ * Näytä muuttujan arvo konsolissa.
+*/
+String nimi = "Pekka";
+Console.WriteLine(nimi);
+Console.WriteLine("Nimesi = " + nimi);
