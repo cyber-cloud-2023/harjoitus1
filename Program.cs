df59fcf633e67bb96cd3e5ab5327cf1a79c9c807
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 11:01:24 +0300

    t3 valmis

--- Program.cs
+++ Program.cs
@@ -15,3 +15,25 @@ Console.WriteLine("Ikä = " + ika);
 String nimi = "Pekka";
 Console.WriteLine(nimi);
 Console.WriteLine("Nimesi = " + nimi);
+
+/*Tehtävä3
+ * Luo ohjelma, joka luo kaksi kokonaislukumuuttujaa, 
+ * "luku1" ja "luku2", ja antaa niille eri arvot. 
+ * Suorita yhteen-, vähennys-, kerto- ja jakolaskut 
+ * käyttäen näitä muuttujia ja näytä tulokset konsolissa.
+ */
+int luku1=5;
+int luku2=2;
+int tulos;
+
+tulos=luku1 + luku2;
+Console.WriteLine("Lukujen "+luku1+" ja "+luku2+" summa = "+tulos);
+
+tulos = luku1 - luku2;
+Console.WriteLine("Lukujen " + luku1 + " ja " + luku2 + " erotus = " + tulos);
+
+tulos = luku1 * luku2;
+Console.WriteLine("Lukujen " + luku1 + " ja " + luku2 + " tulo = " + tulos);
+
+float osam = luku1 / (float)luku2;
+Console.WriteLine("Lukujen " + luku1 + " ja " + luku2 + " osamäärä = " + osam);
